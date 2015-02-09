d94c6afe394bd9f97f9c5ff79ae7076028cf3dd8
npranke <nicole@npranke.io>
2015-02-09 15:57:23 -0500

    Change hello.py to include recursive factorial function

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
 print 'hello, folks'
 print "adding 10 and 5: " + str(10 + 5)
+
+def factorial(n):
+	if n = 0:
+		1
+	elif n = 1:
+		1
+	else:
+		factorial(n - 1)
+
+print "this is 8!: " + str(factorial(8))
